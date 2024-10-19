4843f3ee25fac4bfd0a74b1f2a2849ffd2289dcc
hannahmyears <hannmyrs@gmail.com>
2024-10-19 10:50:22 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,25 @@
 package hello;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class HelloWorld 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		
 		Scanner k = new Scanner(System.in);
-		System.out.println("Input a thing: ");
-		Statement statement = new Statement(k.next());
+		System.out.println("Input the file you'd like read: ");
+		
+		File file = new File(k.nextLine());
+		
+		String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+		String[] lines = content.split("\\r?\\n");
+		
+		Statement statement = new Statement();
 		
 	}
 }

96d4f6b8ba04c5b57d26fd990e5fb596887b818c
hannahmyears <hannmyrs@gmail.com>
2024-10-19 10:08:38 -0400

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,59 @@
+package hello;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class Frame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame frame = new Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(33, 50, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(123, 132, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(265, 80, 61, 16);
+		contentPane.add(lblNewLabel);
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+import java.util.Scanner;
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{
+		
+		Scanner k = new Scanner(System.in);
+		System.out.println("Input something: ");
+		String word = k.next();
+		
+	}
+}

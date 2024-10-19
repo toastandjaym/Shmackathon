57cfdae057cbc5bdc837e0844ba6400ce53197c3
hannahmyears <hannmyrs@gmail.com>
2024-10-19 11:05:59 -0400

    Update Statement.java

--- Statement.java
+++ Statement.java
@@ -1,14 +1,44 @@
 package hello;
 
 public class Statement {
-	String state;
+	String statement, pointname, qualifier, atrisknotes, followup, datetime;
+	Classification classification;
+	int observationnum;
 
 	public Statement() {
 		
 	}
 	
 	public Statement(String s) {
-		state = s;
+		statement = s;
+		String[] parts = statement.split(",\\s*");
+		observationnum = Integer.parseInt(parts[0]);
+		datetime = parts[1];
+		pointname = parts[2];
+		qualifier = parts[3];
+		atrisknotes = parts[4];
+		followup = parts[5];
+		
+	}
+
+	public boolean highEnergyPresent() {
+		
+		return false;
+	}
+	
+	public boolean highEnergyIncident() {
+		
+		return false;
+	}
+	
+	public boolean directControl() {
+		
+		return false;
+	}
+	
+	public boolean seriousInjury() {
+		
+		return false;
 	}
 	
 }
